154fe2f92b4e9211153c43f65d85b82d5d5849ac
Mirko Lenz <info@mirko-lenz.de>
2022-02-08 00:06:03 +0100

    Refactor typescript index

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,10 +1,8 @@
-module.exports = {
-  aduProto: require("./arg_services/adu/v1/adu_pb"),
-  aduService: require("./arg_services/adu/v1/AduServiceClientPb"),
-  baseProto: require("./arg_services/base/v1/base_pb"),
-  entailmentProto: require("./arg_services/entailment/v1/entailment_pb"),
-  entailmentService: require("./arg_services/entailment/v1/EntailmentServiceClientPb"),
-  graphProto: require("./arg_services/graph/v1/graph_pb"),
-  retrievalProto: require("./arg_services/retrieval/v1/retrieval_pb"),
-  retrievalService: require("./arg_services/retrieval/v1/RetrievalServiceClientPb"),
-};
+export * as aduService from "./arg_services/adu/v1/AduServiceClientPb";
+export * as aduModel from "./arg_services/adu/v1/adu_pb";
+export * as baseModel from "./arg_services/base/v1/base_pb";
+export * as entailmentService from "./arg_services/entailment/v1/EntailmentServiceClientPb";
+export * as entailmentModel from "./arg_services/entailment/v1/entailment_pb";
+export * as graphModel from "./arg_services/graph/v1/graph_pb";
+export * as retrievalService from "./arg_services/retrieval/v1/RetrievalServiceClientPb";
+export * as retrievalModel from "./arg_services/retrieval/v1/retrieval_pb";
